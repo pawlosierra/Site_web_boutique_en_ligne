874569042bd43a71c646ea5deee19c10e2352a87
pawlosierra@hotmail.com <juanpawlosierraarias@MacBook-Pro-de-JUAN.local>
2020-08-17 16:00:26 -0400

    Site Web boutique en ligne

---
+++ js/boutique.js
@@ -0,0 +1,196 @@
+﻿var tab_produits = [];
+var panier = [];
+//var listedeProduit = [];
+i=0;
+
+window.onload=function() {
+  chargerProduits();
+};
+
+//La fonction chargeProduits() creera un tableau associatif ayant comme cle le nom de l'image. Cette fonction retournera tab_produits.
+function chargerProduits(){
+
+  tab_produits["canon-t8i.jpg"] = 1199.00;
+  tab_produits["canon-90D.jpg"] = 1999.99;
+  tab_produits["nikon-z50.jpg"] = 1599.00;
+  tab_produits["flash-godox.jpg"] = 229.95;
+  tab_produits["objectif-canon.jpg"] = 669.00;
+  tab_produits["objectif-canon-macro.jpg"] = 2899.00;
+  tab_produits["trepied.jpg"] = 239.95;
+  tab_produits["sac.jpg"] = 69.95;
+  tab_produits["carte-memoire.jpg"] = 52.95;
+
+  return tab_produits;
+}
+
+//la fonction ajouterAuPanier recevra comme paramètre item. La fonction montre tous les éléments dans le panier d'achat a gauche. Il est nécessaire de créer un élément tr contenant un elements img qui montre le produit et un element td que montre le contenu.
+function ajouterAuPanier(item) {
+  
+  var tr = document.createElement("tr");
+  var img = document.createElement("img");
+  var td = document.createElement("td");
+  img.src = "images/"+item;
+  img.width = "50";
+  img.height = "50";
+
+  var produits= chargerProduits();
+  var prix = produits.includes(item);
+  var prix = produits[item];
+  //Avec la fonction itemExistant, nous vérifions si le produit existe dans le panier d'achat, si le produit n'existe pas, il sera ajouté à notre tableau appelé panier.
+  if(itemExistant(item)){
+    
+  }else{
+  panier.push(item);
+  
+  var texte = document.createTextNode(prix);
+  document.getElementById("bodyPanier").appendChild(tr);
+  tr.onclick = retirerDuPanier;
+  tr.appendChild(img);
+  td.appendChild(texte);
+  tr.appendChild(td);
+  
+  mettreJourPrix();
+  }
+}
+//Avec cette fonction, nous cherchons à actualiser le montant total de l'achat. Cela doit être fait car l'utilisateur peut ajouter ou retirer les produits du panier à tout moment. Cette fonction permet de restituer le prix d'achat total.
+function mettreJourPrix(){
+  var prixProduit=0;
+ var achatTotal=0;
+ //Nous devons passer par le tableau appelé "panier" pour vérifier tous les produits que nous y avons.
+ for(var n=0; n<panier.length;n++){
+   //nous avons 9 produits différents avec leurs prix respectifs. Dans la table du panier, il y a les produits que le client veut acheter. En utilisant un Switch, vous pouvez obtenir le prix de chacun des produits dans le panier et en même temps les additionner pour obtenir un total.
+   switch (panier[n]){
+     
+    case "canon-t8i.jpg":
+      prixProduit = 1199.00;
+      achatTotal = achatTotal + prixProduit;
+      break;
+    case "canon-90D.jpg":
+      prixProduit = 1999.00;
+      achatTotal = achatTotal + prixProduit;
+      break;
+    case "nikon-z50.jpg":
+      prixProduit = 1599.00;
+      achatTotal = achatTotal + prixProduit;
+      break;
+    case "flash-godox.jpg":
+      prixProduit = 229.95;
+      achatTotal = achatTotal + prixProduit;
+      break;
+    case "objectif-canon.jpg":
+      prixProduit = 669.00;
+      achatTotal = achatTotal + prixProduit;
+      break;
+    case "objectif-canon-macro.jpg":
+      prixProduit = 2899.00;
+      achatTotal = achatTotal + prixProduit;
+      break;
+    case "trepied.jpg":
+      prixProduit = 239.95;
+      achatTotal = achatTotal + prixProduit;
+      break;
+    case "sac.jpg":
+      prixProduit = 69.95;
+      achatTotal = achatTotal + prixProduit;
+      break;
+    case "carte-memoire.jpg":
+      prixProduit = 52.95;
+      achatTotal = achatTotal + prixProduit;
+      break;
+    default:
+      console.log(prixProduit=0);
+   }
+   
+ } 
+ prixTotal = achatTotal.toString();
+ afficherTotal(prixTotal);
+}
+
+//la fonction retireDuPanier vous permet de retirer des produits du panier. Cette fonction est appelée chaque fois que l'utilisateur clique sur les produits dans le panier.
+function retirerDuPanier(){
+  
+  elementSupprime = this.parentNode.removeChild(this);
+  articleretirepanier = this.firstChild.src;
+  article = articleretirepanier.substring(91);
+  positionpanier = panier.indexOf(article);
+  panier.splice(positionpanier,1);
+  //La fonction mettreJourPrix() doit être appelée pour mettre à jour la valeur d'achat.
+  mettreJourPrix();
+}
+
+
+//Avec la fonction afficherTotal, nous pouvons montrer au client le prix total des produits dans le panier.
+function afficherTotal(prixTotal){
+  document.getElementById("divTotalPanier").innerHTML = prixTotal;
+}
+
+/*  La fonction itemExistant valide si l'item est déjà dans le panier et
+ *  retourne true s'il est présent, et false s'il ne l'est pas  */
+function itemExistant(item){
+
+  if (panier.includes(item)){
+    alert("Un item ne doit pas pouvoir se retrouver deux fois dans la liste.");
+    return true;
+  }else{
+    
+    return false;
+  }
+    
+}
+//La fonction validerPaiement vérifiera si l'utilisateur saisit correctement ses données personnelles. Par exemple, il vérifiera que le nom n'a pas d'espace au début et à la fin. Il vérifiera également que le numéro de la carte de crédit est uniquement Masterd Card et Visa. En outre, nous vérifierons que le numéro de sécurité de la carte (CVC) ne contient que 3 caractères, qui doivent tous être des chiffres. Il est important de noter qu'aucun champ ne doit être vide.
+function validerPaiement(f){
+  var nom = f.nom.value.trim(); 
+  if (nom =="") {
+      alert("Le nom est obligatoire.");
+      return false;
+   }
+  if (nom.length<3){
+      alert("Le nom doit etre plus long que 3 caracteres.");
+      return false;
+  }
+  var numero = f.numero.value.trim(); 
+  if (numero =="") {
+      alert("Le numero est obligatoire.");
+      return false;
+   }
+  var numeroString = numero.toString();
+    if (!(numero.charAt(0) === "4")){
+      if (!(numero.charAt(0) === "5")){
+        alert("Seulement les cartes Visa et MasterCard sont acceptes.");
+        return false;
+      }
+  }
+  var cvc = f.cvc.value.trim(); 
+  if (cvc =="") {
+      alert("Le cvc est obligatoire.");
+      return false;
+   }
+  //nous devons convertir le numéro CVC en une chaîne de caractères afin de vérifier que ce numéro de sécurité comporte 3 chiffres.
+  var cvcString = cvc.toString();
+  if (!(cvcString.length === 3)){
+    alert("Le code CVC doit avoir 3 chiffres.");
+    return false;
+  }
+  
+    console.log(cvc.length);
+//Nous vérifions que chacun des caractères de la table CVC sont des chiffres de 0 à 9.
+    for(var c=0; c<cvc.length;c++ ){
+      if ((( cvc[c] === '0')) || (( cvc[c] === '1')) || (( cvc[c] === '2'))|| (( cvc[c] === '3'))|| (( cvc[c] === '4'))|| (( cvc[c] === '5'))|| (( cvc[c] === '6'))|| (( cvc[c] === '7'))|| (( cvc[c] === '8'))|| (( cvc[c] === '9'))){
+          console.log(cvc[c]);
+      }else{
+        alert("Le code CVC doit etre des chiffres.");
+        return false;
+      }
+    }
+  
+    //nous avons créé les tableaux des mois et des jours. Ensuite, nous créons l'objet date. Nous faisons tout cela pour montrer avec une fenêtre que l'achat a été fait correctement en concaténant cette information avec la date à laquelle l'achat a été fait.
+  var mois = new Array("janvier","fevrier","mars","avril","mai","juin","juilliet","aout","septembre","octubre","novembre","decembre");
+  var jour = new Array("dimanche","lundi", "mardi","mercredi","jeudi","vendredi","samedi");
+
+  var date = new Date();
+  alert("Paiement effectue avec succès le "+jour[date.getDay()]+" "+ date.getDate()+" "+mois[date.getMonth()]+" "+date.getFullYear()+" "+"a"+" "+date.getHours()+":"+date.getMinutes());
+
+  return true;
+
+
+}
---
+++ js/repertoire.js
@@ -0,0 +1,191 @@
+﻿
+//la déclaration et l'initialisation de la table d'objets nommée tab_photographes est effectuée.
+var tab_photographes = []
+
+//La fonction Photographe est le constructeur. 
+//nom, region, type sont les attributs de l'objet.
+function Photographe(nom, region, type) {
+  this.nom = nom;
+  this.region = region;
+  this.type = type;
+}
+
+window.onload=function() {
+  chargerPhotographes();
+  document.getElementById("rafraichir").addEventListener("click", rafraichir);
+  //le évènement doit ecouter tout changement (change) sur le select#filtrer-region. Apres il 
+  //vas executer la fonction fitrerParRegion().
+  document.getElementById("filtrer-region").addEventListener("change", filtrerParRegion);
+  //le évènement doit écouter tout changement (change) sur le select#filtrer-type. Apres il vas
+  //exécuter la function filtrerParType().
+  document.getElementById("filtrer-type").addEventListener("change", filtrerParType);
+  //le évènement doit écouter le déclenchement du keyup sur le input#nom et executer la fonction
+  //filtrerParNom().
+  document.getElementById("nom").addEventListener("keyup",filtrerParNom);
+};
+
+//Dans la fonction chargerPhotographes(), nous allons créer les objets qui sont inclus dans 
+//le tableau tab_photographes.
+
+function chargerPhotographes(){
+   
+  var photographe1 = new Photographe("Annie Simard", "Montreal", ["Evenement", "Corporatif"]);
+  var photographe2 = new Photographe("Joey Michaud", "Sherbrooke", ["Corporatif"]);
+  var photographe3 = new Photographe("Micheal", "Quebec", ["Famille"]);
+  var photographe4 = new Photographe("Stephanie Dulude", "Quebec", ["Mariage"]);
+  var photographe5 = new Photographe("Isabelle Theriault", "Quebec", ["Famille", "Mariage"]);
+  var photographe6 = new Photographe("Magalie Simoneau", "Montreal", ["Mariage"]);
+  var photographe7 = new Photographe("John Harvey", "Trois-Riviere", ["Corporatif"]);
+  //Avec la fonction push(), les objets créés dans le tableau des tab_photographes sont saisis un par un.
+  tab_photographes.push(photographe1);
+  tab_photographes.push(photographe2);
+  tab_photographes.push(photographe3);
+  tab_photographes.push(photographe4);
+  tab_photographes.push(photographe5);
+  tab_photographes.push(photographe6);
+  tab_photographes.push(photographe7);
+
+  //nous appelons la fonction afficherPhotographes afin qu'elle affiche à l'écran le tableau des objets que //nous avons créés.
+  afficherPhotographes(tab_photographes);
+
+}
+
+  //la fonction afficherPhotographes montre tous les éléments du tableau tab_photographes. Il est nécessaire de créer un élément tr contenant 3 elements td qui montre le contenu de chacun des objets. Il est important de noter que cette fonction affichera tous les objets qui font partie du tableau tab_photographe. C'est-à-dire qu'il affichera toutes les photographes par défaut.
+function afficherPhotographes(tableau){ 
+  
+    for(var i=0; i<tableau.length;i++){
+    var tr = document.createElement("tr");
+    var nom = document.createElement("td");
+    var ville = document.createElement("td");
+    var type = document.createElement("td");
+    var texte = document.createTextNode(tableau[i].nom);
+    var texte2 = document.createTextNode(tableau[i].region);
+    var texte3 = document.createTextNode(tableau[i].type);
+
+    nom.appendChild(texte);
+    ville.appendChild(texte2);
+    type.appendChild(texte3);
+
+    tr.appendChild(nom);
+    tr.appendChild(ville);
+    tr.appendChild(type);
+
+    document.getElementById("tableau-photographes").appendChild(tr).setAttribute('id',i);
+    
+    }
+}
+
+//Dans la fonction filtrerParType, le filtrage sera effectué en tenant compte de l'événement "filtrer par type". En fonction de la sélection de l'utilisateur, le tableau tab_photographes sera filtré et seuls les éléments correspondant à la recherche seront affichés à partir du tableau tab_photographes. 
+
+function filtrerParType(e){
+
+  //Dans la variable demandeFiltrage nous stockons une variable de type chaîne de caractères dans laquelle nous allons stocker l'option que l'utilisateur a sélectionnée comme filtrage.
+  demandeFiltrage = e.target.value;
+  
+  var tabType = [];
+  //Tant que l'utilisateur ne sélectionne pas le type de filtrage par type, l'ensemble du tableau d'objets sera affiché.
+  if(demandeFiltrage!=""){
+    for(var t=0; t<tab_photographes.length;t++){
+      //dans le tableau tabType, nous allons stocker uniquement les éléments de type de chacun des objets du tableau. C'est-à-dire que nous créons un nouveau tableau où ne figurent que des informations comme: famille, Corporatif, etc ...
+      tabType[t]=tab_photographes[t].type;
+
+      //Maintenant que nous disposons d'un tableau des types d'informations, nous pouvons effectuer un filtrage. Pour cela, nous utilisons la fonction includes(). Le paramètre que nous allons utiliser dans cette fonction est la variable demandeFiltrage.
+      
+      if(tabType[t].includes(demandeFiltrage)){
+
+        document.getElementById(t).style.display="table-row";
+      }else{
+        //si le paramètre sélectionné par l'utilisateur dans le tableau tabType n'est pas inclus, cet élément sera automatiquement masqué.
+        document.getElementById(t).style.display="none";
+      }
+    } 
+  }else{
+    for(var j=0;j<tab_photographes.length;j++){
+      document.getElementById(j).style.display="table-row";
+    }
+  }
+}
+
+//Dans la fonction filtrerParRegion, le filtrage sera effectué en tenant compte de l'événement "filtrer par region". En fonction de la sélection de l'utilisateur, le tableau tab_photographes sera filtré et seuls les éléments correspondant à la recherche seront affichés à partir du tableau tab_photographes. 
+function filtrerParRegion(e){
+   //Dans la variable demandeFiltrage nous stockons une variable de type chaîne de caractères dans laquelle nous allons stocker l'option que l'utilisateur a sélectionnée comme filtrage.
+  demandeFiltrage = e.target.value;
+  //Tant que l'utilisateur ne sélectionne pas le type de filtrage par region, l'ensemble du tableau d'objets sera affiché.
+  //nous utilisons "" lorsque l'utilisateur ne sélectionne aucune ville.
+  if(demandeFiltrage!=""){
+  
+  tabVille = [];
+  for(var v=0; v<tab_photographes.length;v++){
+    //un tableau appelé tabVille est créé où seules les villes de chacun des objets sont stockées. 
+    tabVille[v]=tab_photographes[v].region;
+  } 
+
+  var compteur = [];
+  var k = 0;
+  for(var j=0; j<tabVille.length;j++){
+    //Nous vérifions si l'élément que l'utilisateur a sélectionné dans le tableau des tabVille existe, c'est-à-dire une ville. Nous créons un tableau appelé compteur où nous stockons l'index de la table tabVille où nous trouvons la ville que l'utilisateur a sélectionnée.
+    if(tabVille[j] == demandeFiltrage){
+      compteur[k] =j;
+      k=k+1;
+    }
+  }
+  var tableau=[];
+  for(var p=0;p<tab_photographes.length;p++){
+    //nous utilisons chacune des valeurs du tableau compteur comme indice. 
+    //Seuls les objets dont l'index est présent en compteur seront stockés dans le tableau.
+  tableau [p]= tab_photographes[compteur[p]];
+  }
+  //Avec la fonction filter(), nous allons nettoyer les tableaux dans lesquels il y a des éléments non définis.
+  tableauRase = tableau.filter(Boolean);
+  tableau = tableauRase;
+  for(var j=0;j<tab_photographes.length;j++){
+    ////Maintenant que nous disposons d'un tableau compteur, nous pouvons effectuer un filtrage. Pour cela, nous utilisons la fonction includes(). Le paramètre que nous allons utiliser dans cette fonction est la variable j. Nous voulons vérifier que les index de la table des compteurs sont dans la table tab_photographes.
+   if(compteur.includes(j)){
+    document.getElementById(j).style.display="table-row";
+   }else{
+     //si le paramètre sélectionné par l'utilisateur dans le tableau compteur n'est pas inclus, cet élément sera automatiquement masqué.
+    document.getElementById(j).style.display="none";
+   }  
+  } 
+  }else{
+    for(var j=0;j<tab_photographes.length;j++){
+      document.getElementById(j).style.display="table-row";
+    }
+  }
+  //Comme nous pouvons le vérifier dans cette fonction, la logique et la procédure sont un peu plus étendues que dans les autres fonctions de filtrage. En effet, c'est la première fonction qui a été exécutée et à partir de là, la logique a été améliorée pour les deux autres fonctions.
+}
+
+//Cette fonction doit trouver les photographes qui contiennent la chaine de caractere dans leur nom. 
+
+function filtrerParNom(e){
+  
+   //Dans la variable demandeFiltrage nous stockons une variable de type chaîne de caractères dans laquelle nous allons stocker l'option que l'utilisateur a sélectionnée comme filtrage.
+  demandeFiltrage = e.target.value;
+  //Comment la recherche ne doit pas etre sensible au majuscule ou mminuscule. Vous devez utiliser la fonction toLowerCase() pour convertir toutes les chaînes de caractères en minuscules.
+  demandeFiltrageMinuscule = demandeFiltrage.toLowerCase();
+  var tabNom = [];
+    if(demandeFiltrage!=""){
+      for(var n=0; n<tab_photographes.length;n++){
+        //Seuls les noms d'objets seront stockés dans la table taNom. 
+        tabNom[n]=tab_photographes[n].nom.toLowerCase();
+        //Maintenant que nous disposons d'un tableau des noms, nous pouvons effectuer un filtrage. Pour cela, nous utilisons la fonction includes(). Le paramètre que nous allons utiliser dans cette fonction est la variable demandeFiltrage, c'est-à-dire le nom du photographe que l'utilisateur veut voir à l'écran.
+        if(tabNom[n].includes(demandeFiltrageMinuscule)){
+          document.getElementById(n).style.display="table-row";
+        }else{
+          //si le paramètre sélectionné par l'utilisateur dans le tableau tabNom n'est pas inclus, cet élément sera automatiquement masqué.
+          document.getElementById(n).style.display="none";
+        }
+      }
+      console.log(tabNom);
+  
+    }else{
+      for(var j=0;j<tab_photographes.length;j++){
+        document.getElementById(j).style.display="table-row";
+      }
+    }
+
+}
+
+function rafraichir(){
+  location.reload(); 
+}
